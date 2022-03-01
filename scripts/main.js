93703fb42707b9d976c678f765f9d0a6a6cbc257
weirdyang <wy4786@gmail.com>
2022-03-01 23:01:55 +0800

    updating nabeisms

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,40 @@
 import Splitting from "splitting";
 
 
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
   const nabei = document.getElementById('nabei');
   const titleStart = document.getElementById('title-start');
   const titleEnd = document.getElementById('title-end');
-  let display = () =>  getSingleElement('nabei-text');
+  let display = () => getSingleElement('nabei-text');
   const container = document.getElementById('text');
-  Splitting({ target: nabei, by: 'chars'})
-  Splitting({ target: titleStart, by: 'words'})
-  Splitting({ target: titleEnd, by: 'chars'})
-  Splitting({ target: display()});
+  Splitting({ target: nabei, by: 'chars' })
+  Splitting({ target: titleStart, by: 'words' })
+  Splitting({ target: titleEnd, by: 'chars' })
+  Splitting({ target: display() });
   document.getElementById('title-middle').classList.add('animate');
   document.getElementById('more').addEventListener('click', () => {
     const newText = document.createElement('p');
     newText.innerText = randomItem(nabeisms);
 
-    Splitting({ target: newText});
+    Splitting({ target: newText });
     newText.classList.add('nabei-text');
     container.removeChild(display());
     container.appendChild(newText);
     newText.classList.add('sliding');
-})
+  })
 });
 const getSingleElement = (className) => document.getElementsByClassName(className)[0];
 const randomItem = (arr) => arr[arr.length * Math.random() | 0];
 const nabeisms = [
- 'when the bus driver see you running for it but he still drives off',
- 'when you finally match with someone on tinder and it is your sister',
+  'when the bus driver see you running for it but he still drives off',
+  'when you finally match with someone on tinder and it is your sister',
   'when the grab driver cancels on you and you are literally 1min late',
   'when you friend is late and he says he is on the way, but he is actually on the way out of his bed',
   'when you have the winning 4d numbers, but you bought the wrong date',
-  'when it rains on your wedding day'
+  'when it rains on your wedding day',
+  'when a fire station burns down',
+  'when a member of PETA wears leather shoes',
+  'when your pilot has a fear of heights',
+  'when someone complains about you complaining',
+  "when the cobbler's kids can't afford shoes"
 ]
